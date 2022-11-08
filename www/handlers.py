1d843363b7adc09488b86fc5b480bf23af6e088c
zhuzzh <zhuzzh@chinatelecom.cn>
2022-11-08 10:31:30 +0800

    添加了博客首页和相关css与js

--- www/handlers.py
+++ www/handlers.py
@@ -1,5 +1,7 @@
+import time
+
 from www.coroweb import get
-from www.models import User
+from www.models import User, Blog
 
 
 @get('/')
@@ -8,4 +10,17 @@ async def index(request):
     return {
         '__template__': 'test.html',
         'users': users
+    }
+
+@get('/1')
+async def index1(request):
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
+    return {
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
