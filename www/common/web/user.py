d853d9fc60a856f2ce60c1fda54644e1007a4e3b
zhuzzh <zhuzzh@chinatelecom.cn>
2023-02-10 10:07:30 +0800

    添加用户登录功能 注册功能完善

--- www/common/web/user.py
+++ www/common/web/user.py
@@ -1,6 +1,11 @@
+import asyncio
 import hashlib
+import logging
 import time
 from www.const import _COOKIE_KEY
+
+# 计算加密的cookie
+from www.models import User
 
 
 def user2cookie(user, max_age):
@@ -12,3 +17,32 @@ def user2cookie(user, max_age):
     s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
     L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
     return '-'.join(L)
+
+
+# 通过解密cookie来判断cookie的有效性，传递值
+@asyncio.coroutine
+def cookie2user(cookie_str):
+    '''
+    Parse cookie and load user if cookie is valid.
+    '''
+    if not cookie_str:
+        return None
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        uid, expires, sha1 = L
+        if int(expires) < time.time():
+            return None
+        user = yield from User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1')
+            return None
+        user.passwd = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
--- www/handlers.py
+++ www/handlers.py
@@ -1,5 +1,6 @@
 import hashlib
 import json
+import logging
 import time
 from random import randint
 
@@ -17,6 +18,55 @@ def register():
     return {
         '__template__': 'register.html'
     }
+
+
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'login.html'
+    }
+
+
+@post('/api/authenticate')
+async def authenticate(*, email, passwd):
+    try:
+        if not email:
+            raise APIValueError('email', 'Invalid email.')
+        if not passwd:
+            raise APIValueError('passwd', 'Invalid password.')
+        users = await User.findAll('email=?', [email])
+
+        if len(users) == 0:
+            raise APIValueError('email', 'Email not exist.')
+        user = users[0]
+        # check passwd:
+        sha1 = hashlib.sha1()
+        sha1.update(user.id.encode('utf-8'))
+        sha1.update(b':')
+        sha1.update(passwd.encode('utf-8'))
+        if user.passwd != sha1.hexdigest():
+            raise APIValueError('passwd', 'Invalid password.')
+        # authenticate ok, set cookie:
+        r = web.Response()
+        r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+        user.passwd = '******'
+        r.content_type = 'application/json'
+        r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
+        logging.info(f"user:{user.name} log in")
+        return r
+    except APIValueError as e:
+        logging.error(e)
+
+
+
+@get('/signout')
+def signout(request):
+    referer = request.headers.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out.')
+    return r
+
 
 @get('/api/user')
 async def get_all_users(request):
@@ -40,8 +90,10 @@ async def create_user(*, email, password, name):
     uid = next_id()
     sha1_passwd = '%s:%s' % (uid, password)
     user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
-                image='https://dn-qiniu-avatar.qbox.me/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
+                image='https://dn-qiniu-avatar.qbox.me/avatar/%s?d=mm&s=120' % hashlib.md5(
+                    email.encode('utf-8')).hexdigest())
     await user.save()
+    logging.info(f"create user successful! username:{user.name}")
     # make session cookie:
     r = web.Response()
     r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
